6355f61dc760d3f1aac533c2589afc3b5aa4b80d
keisuke koshikawa <0421kossy0421@gmail.com>
2018-06-03 12:59:13 +0900

    edit model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,13 @@ class MessagesController < ApplicationController
   def index
 
   end
+
+  def create
+    @message = message.new
+    @message
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(params[:current_user.id], :content)
 end
