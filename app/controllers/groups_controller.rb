fbeab7d98db6c0e9340e9887602d976c62cfc6a5
keisuke koshikawa <0421kossy0421@gmail.com>
2018-06-03 12:17:24 +0900

    edit group view and controller
    commonnize form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+
+  end
 
   def new
     @group = Group.new
@@ -8,22 +13,27 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: "グループを作成しました"
+      redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
   end
 
   def update
-
-  end
-
-  def edit
-
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
