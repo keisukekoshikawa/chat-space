c7a3df312e6825d17e4c7bb00c4d930eadb217d4
keisuke koshikawa <0421kossy0421@gmail.com>
2018-06-12 00:38:09 +0900

    complete chat-main
    rspec

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,8 @@ class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
   def index
-
+     @message = Message.new
+     @group = current_user.groups
   end
 
   def new
@@ -13,27 +14,31 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path, notice: "グループを作成しました"
     else
       render :new
     end
   end
 
+  def edit
+    @group = current_user.groups.find(params[:id])
+  end
+
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: "グループを編集しました"
     else
       render :edit
     end
   end
 
   private
+
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, {user_ids: []})
   end
 
   def set_group
     @group = Group.find(params[:id])
   end
-
 end
