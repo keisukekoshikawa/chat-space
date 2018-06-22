78f818b347046b62e883c447ab8651a2335da15b
keisuke koshikawa <0421kossy0421@gmail.com>
2018-06-22 20:00:38 +0900

    return falseとdisabledの追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,26 +2,26 @@ $(document).on("turbolinks:load", function() {
   function buildHTML(message) {
     var img = "";
     if (message.image) {
-      var img = `<img src=${message.image}>`
+      var img = `<img src=${message.image}>`;
     }
     var html =
                `<div class = "contents__chat__message__contents--name">
                   ${message.name}
-                    <div class = "contents__chat__message__contents--timestamp">
-                         ${message.created_at}
-                    </div>
-                    </div>
-                    <div class = "contents__chat__message__contents--messages">
-                      ${message.content}
-                      ${img}
+                  <div class = "contents__chat__message__contents--timestamp">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class = "contents__chat__message__contents--messages">
+                  ${message.content}
+                  ${img}
                 </div>
                `
-    return html
+    return html;
   }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var formData = new FormData(this)
-    var url = $(this).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -33,14 +33,14 @@ $(document).on("turbolinks:load", function() {
 
     .done(function(data) {
       var html = buildHTML(data);
-      $('.contents__chat__message__contents').append(html)
-      $('.contents__footer__input-form--text').val('')
+      $('.contents__chat__message__contents').append(html);
+      $('.contents__chat__message').animate({ scrollTop: $('.contents__chat__message')[0].scrollHeight }, 500);
       $("#new_message")[0].reset();
-      $('.contents__chat__message').animate({ scrollTop: $('.contents__chat__message')[0].scrollHeight }, 500)
+      $(".contents__footer__input-form--button").prop("disabled", false);
     })
     .fail(function() {
-      alert('failed')
+      alert('failed');
     })
+    return false;
   });
 });
-
